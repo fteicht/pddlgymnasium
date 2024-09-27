538bae076b84fe2ccfc18140cd3eb6d1c3ad0a67
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2024-09-27 18:46:23 +0200

    Change version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pddlgymnasium',
-      version='0.0.1',
+      version='0.0.2',
       description='Gym environment based PDDL problem description',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
